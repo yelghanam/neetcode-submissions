e2d04594bb35e2da1dd96c624f357eb5dacfa00a
Yousef ElGhanam <168661290+yelghanam@users.noreply.github.com>
2026-08-10 11:01:42 +0300

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs)
+{
+    //strs = ["act", "pots", "tops", "cat", "stop", "hat"];
+    Dictionary<string, List<string>> dic = new();
+    for (int i=0; i < strs.Length; i++)
+    {
+        char[] chars = strs[i].ToCharArray();// cat->[c,a,t]
+        Array.Sort(chars);
+        string sorted = new string(chars); //   -->[a,c,t]
+        if (dic.ContainsKey(sorted))
+        {
+            dic[sorted].Add(strs[i]);
+        }
+        else 
+        { 
+            dic[sorted] = new List<string>();
+            dic[sorted].Add(strs[i]);
+        }
+    }
+        foreach (List<string> group in dic.Values)
+        {
+        //Console.Write("[")
+            List<List<string>> result = dic.Values.ToList();
+    }
+        return dic.Values.ToList();
+
+    //return [];
+
+}
+}
