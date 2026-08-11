2197ec5c565611306bfe6bf7bf5efc86d6f0ea74
Yousef ElGhanam <168661290+yelghanam@users.noreply.github.com>
2026-08-11 07:25:11 +0300

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cs
@@ -0,0 +1,20 @@
+public class Solution {
+     public bool hasDuplicate(int[] nums) { 
+    
+    HashSet<int> hash = new();
+
+    for (int i= 0; i < nums.Length; i++) {
+
+    if (!hash.Add(nums[i]))
+    {
+        return true;
+    }
+    
+    
+    
+}
+        return false;
+ 
+    
+}
+}
